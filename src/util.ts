5306a87ec99183a793995899a66015156c816a83
neo <arklove@qq.com>
2020-06-03 21:06:23 +0800

    feat: update code style

--- src/util.ts
+++ src/util.ts
@@ -2,28 +2,31 @@
 import simpleGit from 'simple-git';
 
 export function getLastCommitLog(gitProject: string): Promise<any> {
-  return new Promise(function (resolve, reject) {
+  return new Promise((resolve, reject) => {
     const git = simpleGit(gitProject);
 
-    git.log({
-      n: 1
-    }, function (error: any, listLogSummary: any) {
-      if (!error) {
-        resolve(listLogSummary.latest);
-      } else {
-        reject(error);
+    git.log(
+      {
+        n: 1,
+      },
+      (error: any, listLogSummary: any) => {
+        if (!error) {
+          resolve(listLogSummary.latest);
+        } else {
+          reject(error);
+        }
       }
-    });
+    );
   });
 }
 
-export function getPackageName(type: string): string {
-  if (name === "__FULL__") {
-    return "整包"
-  } else if (name === "__APP__") {
-    return "主包"
+export function getPackageName(name: string): string {
+  if (name === '__FULL__') {
+    return '整包';
+  } else if (name === '__APP__') {
+    return '主包';
   } else {
-    return "分包"
+    return '分包';
   }
 }
 
@@ -42,6 +45,7 @@ export function getFormatFileSize(bytes: number) {
   const i = Math.floor(Math.log(bytes) / Math.log(K));
 
   return {
+    // eslint-disable-next-line no-restricted-properties
     size: parseFloat((bytes / Math.pow(K, i)).toFixed(2)),
     measure: MEASURE[i],
   };
